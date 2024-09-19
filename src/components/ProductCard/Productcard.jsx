2d0741f2488ee71b67419619b70d6baeb9662f3e
hemangdigitalxplode <hemang@digitalxplode.com>
2024-09-19 22:05:14 +0530

    Home page carousel and responsive product card

--- src/components/ProductCard/Productcard.jsx
+++ src/components/ProductCard/Productcard.jsx
@@ -25,8 +25,8 @@ const Productcard = ({productImage,productLabel1,productLabel2,productTitle,prod
   return (
     <>
       {/* Main Product card */}
-      <div className="h-auto w-[326px] bg-[#e5e5e5] shadow-md rounded-md overflow-hidden flex flex-col  justify-center p-4">
-        <img style={{width:'100%'}} src={productImage} alt="" />
+      <div className="h-auto w-full sm:w-[48%] lg:w-[326px] bg-[#e5e5e5] shadow-md rounded-md overflow-hidden flex flex-col justify-center p-4">
+        <img className="mx-auto w-[50%] md:w-[100%]" src={productImage} alt="" />
         {/* Product Labels */}
         <div className="productLabels flex gap-2">
           <span className="px-2 py-1 rounded-lg text-[10px] bg-green-500">{productLabel1}</span>
@@ -34,8 +34,8 @@ const Productcard = ({productImage,productLabel1,productLabel2,productTitle,prod
         </div>
         {/* Product Name & Description */}
         <div className="productdesc flex flex-col">
-          <h1 className=" font-bold mt-2 text-[16px]">{truncateTitle(productTitle, 10)}</h1>
-          <p className="text-[14px] text-[#585757] mt-2">{truncateDescription(productDescription, 26)}</p>
+          <h1 className="font-bold mt-2 text-[13px] sm:text-[16px]">{truncateTitle(productTitle, 10)}</h1>
+          <p className="text-[10px] sm:text-[14px] text-[#585757] mt-2">{truncateDescription(productDescription, 26)}</p>
         </div>
         {/* Reviews div */}
         <div className="flex gap-2 mt-2 justify-start items-center">
@@ -46,11 +46,10 @@ const Productcard = ({productImage,productLabel1,productLabel2,productTitle,prod
         <div className="flex gap-2 w-full items-center justify-between mt-4">
           <span className="text-[19px] font-semibold">{productPrice}</span>
           <div className="flex gap-5 items-center">
-            <button className="px-2 py-1 rounded-md bg-white">Add to Cart</button>
+            <button className="px-1 sm:px-2 text-[10px] sm:text-[12px] py-1 rounded-md bg-white">Add to Cart</button>
             <FaHeart size={25} color="red"/>
           </div>
         </div>
-
       </div>
 
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import img1 from '../components/ProductCard/2.png'
 import img2 from '../components/ProductCard/2.png'
 import img3 from '../components/ProductCard/2.png'
 import img4 from '../components/ProductCard/2.png'
+import Carousel from 'react-multi-carousel'
 
 const dummyData = [
   {
@@ -14,8 +15,8 @@ const dummyData = [
     desc:'Step up your style with Luxuras Classic Red High-Top Sneakers. Crafted with durable canvas and a comfortable rubber sole, these unisex shoes offer timeless design and all-day comfort.',
     reviews:'4.95 (4443 Reviews)',
     price:'$ 24',
-    label1:'Trendy Footwear',
-    label2:'Unisex Sneaker'
+    label1:'Trendy',
+    label2:'Unisex'
   },
   {
     id: 2,
@@ -25,7 +26,7 @@ const dummyData = [
     reviews: '4.88 (2987 Reviews)',
     price: '$ 29',
     label1: 'Sportswear',
-    label2: 'Running Shoes'
+    label2: 'Running'
   },
   {
     id: 3,
@@ -34,8 +35,8 @@ const dummyData = [
     desc: 'Luxura’s Black Leather Loafers bring elegance and comfort together. Featuring a sleek design with soft leather and a cushioned insole, these loafers are perfect for formal and casual wear.',
     reviews: '4.90 (3821 Reviews)',
     price: '$ 45',
-    label1: 'Formal Footwear',
-    label2: 'Leather Loafers'
+    label1: 'Formal',
+    label2: 'Leather'
   },
   {
     id: 4,
@@ -44,17 +45,40 @@ const dummyData = [
     desc: 'Elevate your street style with Luxura’s White Low-Top Sneakers. These minimalist sneakers are designed for all-day wear with a durable sole and versatile look to match any outfit.',
     reviews: '4.93 (5120 Reviews)',
     price: '$ 35',
-    label1: 'Casual Footwear',
-    label2: 'Street Style Sneakers'
+    label1: 'Casual',
+    label2: 'Street'
   }
 ]
+
+// Product carousel responsive
+const responsiveHome = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 4,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 3,
+    slidesToSlide: 1 // optional, default to 1.
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    slidesToSlide: 1, // optional, default to 1.
+    partialVisibilityGutter: 50
+  }
+};
 
 const Home = () => {
   return (
     <>
       <div className="max-w-[1540px] w-full mx-auto">
         <Banner />
-        <div className="my-5 p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
+        <h1 className=" text-center mt-4 text-[22px] font-semibold sm:text-[32px]">Top Rated Sneakers</h1>
+        <p className="text-center text-[10px] sm:text-[20px] text-[#585757]">Get upto 20% Off on Mens Casual Sneakers</p>
+        {/* Grid layout for showing product cards */}
+        {/* <div className="my-5 p-4 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
           {
             dummyData.map((item,index) => {
               return <Productcard
@@ -69,6 +93,37 @@ const Home = () => {
               />
             })
           }
+        </div> */}
+
+        {/* Home Page carousel */}
+        <div className="my-5">
+          <Carousel
+            swipeable={true}
+            draggable={true}
+            autoPlay={false}
+            centerMode={true}
+            responsive={responsiveHome}
+            autoPlaySpeed={2000}
+            removeArrowOnDeviceType={['tablet', 'mobile']}
+            infinite={true}
+            arrows={true}
+            showDots={false}
+          >
+            {
+              dummyData.map((item,index) => {
+                return <Productcard
+                  key={index}
+                  productImage={item.img}
+                  productLabel1={item.label1}
+                  productLabel2={item.label2}
+                  productTitle={item.title}
+                  productDescription={item.desc}
+                  productReviews={item.reviews}
+                  productPrice={item.price}
+                />
+              })
+            }
+          </Carousel>
         </div>
 
       </div>
